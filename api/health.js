eb73ae42708b5c124ee51e7bce2ccc4943c82060
huzaifawork <mhuzaifatariq7@gmail.com>
2025-07-29 05:16:31 +0500

    Fix Vercel API routes: Convert ES modules to CommonJS for serverless functions

--- api/health.js
+++ api/health.js
@@ -1,4 +1,4 @@
-export default function handler(req, res) {
+module.exports = function handler(req, res) {
   // Only allow GET requests
   if (req.method !== 'GET') {
     return res.status(405).json({
